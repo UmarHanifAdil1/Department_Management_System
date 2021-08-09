1d4befc5ae3ba5cd40ed978b1dbf5867858c890c
Muhammad Umar Hanif <umarhanifadil1060@gmail.com>
2021-08-08 21:01:00 -0700

    make it better

--- my_lms/settings.py
+++ my_lms/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from os import path
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,4 +135,4 @@ STATIC_ROOT = path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
